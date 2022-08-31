fd4326329637cf1bdc0ec9355d937d05ed35bbf4
omarkhairy21@gmail.com <omarkhairy21@gmail.com>
2022-09-01 01:41:23 +0200

    initialize project

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env('DATABASE_SSL', false),
+    },
+    debug: false,
+  },
+});
